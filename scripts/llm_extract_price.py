563e36093d070d933d31307029a45852d9fa80ff
李江维 <179887890@qq.com>
2025-06-07 22:56:02 +0800

    添加 llm_parser

--- scripts/llm_extract_price.py
+++ scripts/llm_extract_price.py
@@ -5,13 +5,14 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../"
 import argparse
 from src.llm_tools_v1.core.config import set_current_llm
 from src.llm_tools_v1.core.logging import setup_logging
-from src.llm_tools_v1.ai.llm_parser import summary
+from src.llm_tools_v1.ai.llm_parser import extract_bidding_price
 from src.llm_tools_v1.crawlers.biddingcsg import BiddingCsgCrawler
 
 def main():
     parser = argparse.ArgumentParser(description="大模型命令行对话工具")
     parser.add_argument("--model", type=str, default="doubao", help="指定大模型名称（如 doubao、deepseek）")
     parser.add_argument("--url", type=str, default="https://www.bidding.csg.cn/zbhxrgs/1200395227.jhtml", help="招标页面URL")
+    parser.add_argument("--price_type", type=int, default=1, help="招标价格类型")
     args = parser.parse_args()
 
     set_current_llm(args.model)
@@ -19,7 +20,7 @@ def main():
 
     crawler = BiddingCsgCrawler()
     html_content = crawler.read_bidding_page(args.url)
-    print(summary(html_content))
+    print(extract_bidding_price(html_content, args.price_type))
 
 if __name__ == "__main__":
     main() 
--- src/llm_tools_v1/ai/llm_parser.py
+++ src/llm_tools_v1/ai/llm_parser.py
@@ -1,12 +1,12 @@
 from ..core.logging import get_logger
 from ..core.config import get_settings
 from ..core.llm import LLMClient
-from ..ai.prompt_templates import SYS_BIDDING_SUMMARY_PROMPT
+from ..ai.prompt_templates import SYS_BIDDING_INFO_PROMPT, SYS_BIDDING_PRICE_TYPE_ONE_PROMPT, SYS_BIDDING_PRICE_TYPE_TWO_PROMPT
 from ..crawlers.biddingcsg import BiddingCsgCrawler
 
 logger = get_logger()
 
-def summary(html_content):
+def extract_bidding_info(html_content: str):
     """调用大模型总结Bidding CSG网页的内容, 提取关键信息.
     
     
@@ -18,8 +18,10 @@ def summary(html_content):
         
     """
     try:
+        logger.info(f"提取招标基本信息")
+        system_prompt = SYS_BIDDING_INFO_PROMPT.format(html_content=html_content)
         messages = [
-            {"role": "system", "content": SYS_BIDDING_SUMMARY_PROMPT.format(html_content=html_content)},
+            {"role": "system", "content": system_prompt},
         ]
 
         resp = LLMClient(model_name="doubao").chat(messages, temperature=0.1)
@@ -27,7 +29,31 @@ def summary(html_content):
     except Exception as ex:
         logger.warning(f"llm_summary 调用大模型出错, 错误信息: {ex}")
 
+def extract_bidding_price(html_content: str, price_type: int = 1):
+    """调用大模型总结Bidding CSG网页的内容, 提取关键信息.
+    
+    
+    Args:
+        user_prompt: 用户输入的提示词
+        
+    Returns:
+        大模型返回的总结结果
+        
+    """
+    try:
+        logger.info(f"提取招标价格, 价格类型: {price_type}")
+        system_prompt = SYS_BIDDING_PRICE_TYPE_ONE_PROMPT.format(html_content=html_content) if price_type == 1 else SYS_BIDDING_PRICE_TYPE_TWO_PROMPT.format(html_content=html_content)
+        messages = [
+            {"role": "system", "content": system_prompt},
+        ]
+
+        resp = LLMClient(model_name="doubao").chat(messages, temperature=0.1)
+        return resp["choices"][0]["message"]["content"]
+    except Exception as ex:
+        logger.warning(f"llm_summary 调用大模型出错, 错误信息: {ex}")
+
+
 if __name__ == "__main__":
     crawler = BiddingCsgCrawler()
     html_content = crawler.read_bidding_page("https://www.bidding.csg.cn/zbhxrgs/1200395227.jhtml")
-    print(summary(html_content))
+    print(extract_bidding_price(html_content))
